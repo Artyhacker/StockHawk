1be78e4a61bbfafbb838fc6813cd584541c38683
artyhacker <artyhacker@outlook.com>
2017-02-13 20:11:35 +0800

    bug fixes

--- app/src/main/java/com/udacity/stockhawk/ui/StockDetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/StockDetailActivity.java
@@ -105,15 +105,14 @@ public class StockDetailActivity extends AppCompatActivity {
     }
 
     private void initLineChart() {
-        Line line = new Line(mPointValues).setColor(Color.parseColor("#FFCD41"));  //折线的颜色（橙色）
+        Line line = new Line(mPointValues).setColor(Color.parseColor("#FFCD41"));
         List<Line> lines = new ArrayList<Line>();
-        line.setShape(ValueShape.CIRCLE);//折线图上每个数据点的形状  这里是圆形 （有三种 ：ValueShape.SQUARE  ValueShape.CIRCLE  ValueShape.DIAMOND）
-        line.setCubic(false);//曲线是否平滑，即是曲线还是折线
-        line.setFilled(false);//是否填充曲线的面积
-//        line.setHasLabels(true);//曲线的数据坐标是否加上备注
-        line.setHasLabelsOnlyForSelected(true);//点击数据坐标提示数据（设置了这个line.setHasLabels(true);就无效）
-        line.setHasLines(true);//是否用线显示。如果为false 则没有曲线只有点显示
-        line.setHasPoints(true);//是否显示圆点 如果为false 则没有原点只有点显示（每个数据点都是个大的圆点）
+        line.setShape(ValueShape.CIRCLE);
+        line.setCubic(false);
+        line.setFilled(false);
+        line.setHasLabelsOnlyForSelected(true);
+        line.setHasLines(true);
+        line.setHasPoints(true);
 
         lines.add(line);
         final LineChartData data = new LineChartData();
@@ -121,24 +120,21 @@ public class StockDetailActivity extends AppCompatActivity {
 
         //坐标轴
         Axis axisX = new Axis(); //X轴
-        axisX.setHasTiltedLabels(true);  //X坐标轴字体是斜的显示还是直的，true是斜的显示
-        axisX.setTextColor(Color.WHITE);  //设置字体颜色
-//        axisX.setName("DATE");  //表格名称
-        axisX.setTextSize(10);//设置字体大小
-        axisX.setMaxLabelChars(8); //最多几个X轴坐标，意思就是你的缩放让X轴上数据的个数7<=x<=mAxisXValues.length
-        axisX.setValues(mAxisXValues);  //填充X轴的坐标名称
-        data.setAxisXBottom(axisX); //x 轴在底部
-        //data.setAxisXTop(axisX);  //x 轴在顶部
-        axisX.setHasLines(true); //x 轴分割线
+        axisX.setHasTiltedLabels(true);
+        axisX.setTextColor(Color.WHITE);
+//        axisX.setName("DATE");
+        axisX.setTextSize(10);
+        axisX.setMaxLabelChars(8);
+        axisX.setValues(mAxisXValues);
+        data.setAxisXBottom(axisX);
+        axisX.setHasLines(true);
 
-        // Y轴是根据数据的大小自动设置Y轴上限(在下面我会给出固定Y轴数据个数的解决方案)
+        // Y轴是根据数据的大小自动设置Y轴上限
         Axis axisY = new Axis();  //Y轴
-        axisY.setName("PRICE");//y轴标注
-        axisY.setTextSize(10);//设置字体大小
-        data.setAxisYLeft(axisY);  //Y轴设置在左边
-//        data.setAxisYRight(axisY);  //y轴设置在右边
+        axisY.setName("PRICE");
+        axisY.setTextSize(10);
+        data.setAxisYLeft(axisY);
 
-        //设置行为属性，支持缩放、滑动以及平移
         lineChart.setInteractive(true);
         lineChart.setZoomType(ZoomType.HORIZONTAL);
         lineChart.setMaxZoom((float) 5);//最大方法比例
